95d55e642667cf94640bb2d8e6d74ebbfd1ecbc4
Ltphuong123 <101852175+Ltphuong123@users.noreply.github.com>
2025-09-29 17:25:04 +0700

    n

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,7 @@ const authMiddleware = require('../middlewares/authMiddleware');
 router.post('/users/register', userController.signup);
 router.post('/users/login', userController.login);
 router.get('/users', authMiddleware.verifyToken, authMiddleware.isAdmin, userController.getAllUsers);
+router.post('/logout', authMiddleware.verifyToken, userController.logout);
 
 
 module.exports = router;
